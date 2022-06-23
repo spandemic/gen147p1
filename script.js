f35a4d211784b0631bd46697f246502ecc140353
spandemic <anloeng@ucsc.edu>
2022-06-22 17:44:53 -0700

    added verb

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const fillers = {
     adventurer: ["Derrick Rose", "Dirk Nowitzky", "Jason Kidd", "John Stockton", "Joel Embiid", "Charles Barkley", "Damian Lillard", "Ray Allen", "Jeremy Lin", "Carmelo Anthony", "Paul George", "Dwayne Wade", "Luka Doncic", "Chris Paul", "Russell Westbrook", "Blake Griffin", "Jayson Tatum", "Reggie Miller", "Jimmy Butler", "James Harden", "Kevin Durant", "Patrick Ewing"],
     bball: ["Steph Curry", "Giannis Antetokounmpo", "Michael Jordan", "Shaquille O' Neal", "Nikola Jokic", "LeBron James", "Kobe Bryant", "Magic Johnson", "Larry Bird", "Tim Duncan", "Kawhi Leonard"],
     referee: ["Tony Brothers", "Zach Zarba", "Bill Kennedy", "Darrell Garretson", "Earl Strom", "Dick Bavetta", "Joey Crawford", "Tim Donaghy"],
+    verb: ["eviscerating", "ravaging", "pillaging", "vaporizing", "eliminating", "executing", "mutilating", "trampling", "gaslighting"],
     city: ["Boston", "Los Angeles", "Seattle", "Detroit", "Brooklyn", "Portland", "Dallas", "San Antonio", "Pheonix", "New Orleans", "Sacramento"],
     adjective: ["windy", "desolate", "barren", "hopeless", "thriving", "esteemed", "racist", "bigoted", "distraught", "war-torn", "cherished", "respected", "forgotten"],
-    people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca"],
+    people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "racist", "bigoted", "trashy", "disrespectful"],
     item: ["arm sleeve", "Jordans", "Headband", "face mask", "ankle wrap", "mouthguard"],
     stat: ["points", "rebounds", "assists", "steals", "blocks", "deflections"],
     statNum: ["5", "10", "15", "20", "25", "30", "35", "40", "45", "50"],
@@ -15,7 +16,7 @@ const fillers = {
   
   const template = `$adventurer, where do you think you're going?!?
   
-  $bball was just seen in the direction you're currently heading in, ravaging all the $people fans in his wake!!!
+  $bball was just seen in the direction you're currently heading in, $verb all of the $people fans in his wake!!!
 
   He was just named a $time time league MVP, and his teammate $bball is averaging over $statNum $stat per game, you stand no chance!
   
