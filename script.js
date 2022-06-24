7d82ef913adb4050e63d751b9deded736072364b
spandemic <anloeng@ucsc.edu>
2022-06-23 20:15:37 -0700

    added more variance

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const fillers = {
-    adventurer: ["Derrick Rose", "Dirk Nowitzky", "Jason Kidd", "John Stockton", "Joel Embiid", "Charles Barkley", "Damian Lillard", "Ray Allen", "Jeremy Lin", "Carmelo Anthony", "Paul George", "Dwayne Wade", "Luka Doncic", "Chris Paul", "Russell Westbrook", "Blake Griffin", "Jayson Tatum", "Reggie Miller", "Jimmy Butler", "James Harden", "Kevin Durant", "Patrick Ewing"],
-    bball: ["Steph Curry", "Giannis Antetokounmpo", "Michael Jordan", "Shaquille O' Neal", "Nikola Jokic", "LeBron James", "Kobe Bryant", "Magic Johnson", "Larry Bird", "Tim Duncan", "Kawhi Leonard"],
-    referee: ["Tony Brothers", "Zach Zarba", "Bill Kennedy", "Darrell Garretson", "Earl Strom", "Dick Bavetta", "Joey Crawford", "Tim Donaghy"],
+    adventurer: ["Derrick Rose", "Dirk Nowitzky", "Jason Kidd", "John Stockton", "Joel Embiid", "Charles Barkley", "Damian Lillard", "Ray Allen", "Jeremy Lin", "Carmelo Anthony", "Paul George", "Dwayne Wade", "Luka Doncic", "Chris Paul", "Russell Westbrook", "Blake Griffin", "Jayson Tatum", "Reggie Miller", "Jimmy Butler", "James Harden", "Kevin Durant", "Patrick Ewing", "Steve Nash", "Allen Iverson", "Yao Ming", "Tracy McGrady"],
+    bball: ["Steph Curry", "Giannis Antetokounmpo", "Michael Jordan", "Shaquille O' Neal", "Nikola Jokic", "LeBron James", "Kobe Bryant", "Magic Johnson", "Larry Bird", "Tim Duncan", "Bill Russell", "Wilt Chamberlain", "George Mikan"],
+    coach: ["Steve Kerr", "Pat Riley", "Doc Rivers", "Phil Jackson", "Ty Lue", "Gregg Popovich", "Brad Stevens", "Eric Spolstra", "Monty Williams", "Chuck Daly", "KC Jones", "Tom Thibodeau", "Mike D'Antoni", "Frank Vogel"],
+    referee: ["Tony Brothers", "Zach Zarba", "Bill Kennedy", "Darrell Garretson", "Earl Strom", "Dick Bavetta", "Joey Crawford", "Tim Donaghy", "Stephen A. Smith", "Max Kellerman"],
     verb: ["eviscerating", "ravaging", "pillaging", "vaporizing", "eliminating", "executing", "mutilating", "trampling", "gaslighting"],
     city: ["Boston", "Los Angeles", "Seattle", "Detroit", "Brooklyn", "Portland", "Dallas", "San Antonio", "Pheonix", "New Orleans", "Sacramento"],
     adjective: ["windy", "desolate", "barren", "hopeless", "thriving", "esteemed", "racist", "bigoted", "distraught", "war-torn", "cherished", "respected", "forgotten"],
     people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "racist", "bigoted", "trashy", "disrespectful"],
-    item: ["arm sleeve", "Jordans", "Headband", "face mask", "ankle wrap", "mouthguard"],
+    item: ["arm sleeves", "Jordans", "Headband", "face mask", "ankle wraps", "mouthguard"],
     stat: ["points", "rebounds", "assists", "steals", "blocks", "deflections"],
     statNum: ["5", "10", "15", "20", "25", "30", "35", "40", "45", "50"],
     time: ["2", "3", "4", "5", "6"],
-    shots: ["threes", "dunks", "midrange shots", "free throws", "layups", "buzzer beaters", "defence"],
+    shots: ["three point shots", "dunks", "midrange shots", "free throws", "layups", "buzzer beaters", "defence"],
     GOAT: ["Maurice Podoloff", "J. Walter Kennedy", "Larry O'Brien", "David Stern", "Adam Silver"]
     };
   
@@ -18,13 +19,17 @@ const fillers = {
   
   $bball was just seen in the direction you're currently heading in, $verb all of the $people fans in his wake!!!
 
-  He was just named a $time time league MVP, and his teammate $bball is averaging over $statNum $stat per game, you stand no chance!
+  He was just named a $time time league MVP, and his teammate $adventurer is averaging over $statNum $stat per game, you stand no chance!
+
+  Led by $coach, no city escapes unscathed.
   
   If you are so persistent in going; I, $referee, will guide you to the $adjective city of $city.
   
-  Bring your most enchanted $item, and may we bring glorious championship pedigree to our $adjective city of $city.
+  Bring your most enchanted $item, and may we bring glorious championship pedigree to $city.
 
-  May $GOAT bless our $shots. AMEN
+  May $GOAT bless our $shots. 
+  
+  AMEN
   `;
   
   
